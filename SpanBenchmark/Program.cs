5fd0fd2079c967be89fe09bb370d51181107059c
Hakan Akdoğan <hakan.akdogan@ozdilek.com.tr>
2023-06-16 16:41:58 +0300

    Span vs string

--- SpanBenchmark/Program.cs
+++ SpanBenchmark/Program.cs
@@ -14,50 +14,53 @@ public class Program
 [MemoryDiagnoser]
 public class Bench
 {
-    string[] products = new string[500];
-    public Bench()
-    {
-        Random rnd = new Random();
-        
-        for (int i = 0; i < products.Length; i++)
-        {
-            int num = rnd.Next(1, 100);
-            string name = GetRandomProductName(rnd);
-            products[i] = $"{num} {name}";
-        }
-        static string GetRandomProductName(Random rnd)
-        {
-            string[] names = { "Laptop", "Televizyon", "Buzdolabı", "Kulaklık", "Monitör", "Klavye", "Fare", "Cep Telefonu", "Kamera", "Hoparlör" };
-            int index = rnd.Next(names.Length);
-            return names[index];
-        }
-    }
+    private string input;
+    private readonly string searchTerm = "example";
+    private readonly string replaceTerm = "sample";
+    private readonly string startsWith = "This";
 
-    [Benchmark]
-    public int SpanCalculate()
-    {
-        var prods = products.AsSpan();
-        var sum = 0;
-        foreach (var item in prods)
-        {
-            var count = Int32.Parse( item.Split(' ')[0]);
-            sum+= count;
-        }
+    private ReadOnlySpan<char> InputSpan => input.AsSpan();
+    private ReadOnlySpan<char> SearchTermSpan => searchTerm.AsSpan();
+    private ReadOnlySpan<char> ReplacetermSpan => replaceTerm.AsSpan();
+    private ReadOnlySpan<char> StarsWithSpan => startsWith.AsSpan();
 
-        return sum;
+    [Params(1000,10000)]
+    public int Iterations { get; set; }
+    [GlobalSetup]
+    public void Setup()
+    {
+        input = string.Concat(Enumerable.Repeat("This is an example sentence", Iterations));
     }
     [Benchmark]
-    public int ListCalculate()
+    public string StringReplace() => input.Replace(searchTerm, replaceTerm);
+    [Benchmark]
+    public string SpanReplace()
     {
-        var prods = products.ToList();
-        var sum = 0;
-        foreach (var item in prods)
-        {
-            var count = Int32.Parse(item.Split(' ')[0]);
-            sum += count;
-        }
-
-        return sum;
+        return ReplaceSpan(InputSpan, SearchTermSpan, ReplacetermSpan).ToString();
     }
+    [Benchmark]
+    public string StringSubString() => input[..20];
 
+    [Benchmark]
+    public string SpanSlice() => InputSpan[..20].ToString();
+
+    [Benchmark]
+    public bool StringStartsWith() => input.StartsWith(startsWith);
+
+    [Benchmark]
+    public bool SpanStartsWith() => InputSpan.StartsWith(StarsWithSpan);
+
+
+    private static ReadOnlySpan<char> ReplaceSpan(ReadOnlySpan<char> source, ReadOnlySpan<char> oldValue, ReadOnlySpan<char> newValue)
+    {
+        int index = source.IndexOf(oldValue);
+        if (index == -1) return source;
+        char[] result = new char[source.Length + newValue.Length - oldValue.Length];
+        Span<char> resultSpan = result;
+
+        source.Slice(0, index).CopyTo(resultSpan);
+        newValue.CopyTo(resultSpan.Slice(index));
+        source.Slice(index+oldValue.Length).CopyTo(resultSpan.Slice((index+oldValue.Length)/2));
+        return resultSpan;
+    }
 }
