f106c3d2fc00b5d58ca95560b973c239fc9eb3f7
Hakan Akdoğan <hakan.akdogan@ozdilek.com.tr>
2023-08-23 16:38:44 +0300

    Replace Span Fixed

--- SpanBenchmark/Program.cs
+++ SpanBenchmark/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
 using BenchmarkDotNet.Running;
+using CommandLine.Text;
 using System.Collections;
 using System.Runtime.InteropServices;
 
@@ -52,56 +53,49 @@ public class Bench
 
     }
     [Benchmark]
-    public string StringReplace() => input.Replace(searchTerm, replaceTerm);
+    public void StringReplace() => input.Replace(searchTerm, replaceTerm);
     [Benchmark]
-    public string SpanReplace()
+    public void SpanReplace() => ReplaceSpan(InputSpan, SearchTermSpan, ReplacetermSpan);
+    
+    //[Benchmark]
+    //public string StringSubString() => input[..20];
+
+    //[Benchmark]
+    //public ReadOnlySpan<char> SpanSlice() => InputSpan[..20];
+
+    //[Benchmark]
+    //public bool StringStartsWith() => input.StartsWith(startsWith);
+
+    //[Benchmark]
+    //public bool SpanStartsWith() => InputSpan.StartsWith(StarsWithSpan);
+    //[Benchmark]
+    //public bool TestString() => input.Equals(searchTerm);
+    //[Benchmark]
+    //public bool TestSpan() => MemoryExtensions.Equals(InputSpan, SearchTermSpan, StringComparison.Ordinal);
+
+    //[Benchmark]
+    //public bool ListContains() => _data.Contains("example");
+    //[Benchmark]
+    //public bool SpanContains() => CollectionsMarshal.AsSpan(_data).Contains("example");
+
+    //[Benchmark]
+    //public bool ArrayListContains() => _dataArray.Contains("example");
+
+    //[Benchmark]
+    //public bool QueueContains() => _queue.Contains("example");
+
+    //[Benchmark]
+    //public bool StackContains() => _stack.Contains("example");
+
+
+
+    private static void ReplaceSpan(ReadOnlySpan<char> source, ReadOnlySpan<char> oldValue, ReadOnlySpan<char> newValue)
     {
-        return ReplaceSpan(InputSpan, SearchTermSpan, ReplacetermSpan).ToString();
-    }
-    [Benchmark]
-    public string StringSubString() => input[..20];
-
-    [Benchmark]
-    public string SpanSlice() => InputSpan[..20].ToString();
-
-    [Benchmark]
-    public bool StringStartsWith() => input.StartsWith(startsWith);
-
-    [Benchmark]
-    public bool SpanStartsWith() => InputSpan.StartsWith(StarsWithSpan);
-    [Benchmark]
-    public bool TestString() => input.Equals(searchTerm);
-    [Benchmark]
-    public bool TestSpan() => MemoryExtensions.Equals(InputSpan, SearchTermSpan, StringComparison.Ordinal);
-
-    [Benchmark]
-    public bool ListContains() => _data.Contains("fsdgasd");
-    [Benchmark]
-    public bool SpanContains()
-    {
-        return CollectionsMarshal.AsSpan(_data).Contains("fsdgasd");
-    }
-    [Benchmark]
-    public bool ArrayListContains() => _dataArray.Contains(_data);
-
-    [Benchmark]
-    public bool QueueContains() => _queue.Contains("fsdgasd");
-
-    [Benchmark]
-    public bool StackContains() => _stack.Contains("fsdgasd");
-
-
-
-    private static ReadOnlySpan<char> ReplaceSpan(ReadOnlySpan<char> source, ReadOnlySpan<char> oldValue, ReadOnlySpan<char> newValue)
-    {
-        int index = source.IndexOf(oldValue);
-        if (index == -1) return source;
-        char[] result = new char[source.Length + newValue.Length - oldValue.Length];
-        Span<char> resultSpan = result;
-
-        source.Slice(0, index).CopyTo(resultSpan);
-        newValue.CopyTo(resultSpan.Slice(index));
-        source.Slice(index+oldValue.Length).CopyTo(resultSpan.Slice((index+oldValue.Length)/2));
-        return resultSpan;
+        int indexOfSearch = source.IndexOf(oldValue);
+        int resultLength = source.Length + (newValue.Length - oldValue.Length);
+        Span<char> result = stackalloc char[resultLength];
+        source.Slice(0, indexOfSearch).CopyTo(result);
+        newValue.CopyTo(result.Slice(indexOfSearch));
+        source.Slice(indexOfSearch + oldValue.Length).CopyTo(result.Slice(indexOfSearch + newValue.Length));
     }
 }
